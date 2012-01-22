9cdc6c5ea9e11d34ef9f6026991edc6578481ff0
Brandon Barry <maydaytx@gmail.com>
2012-01-21 21:17:03 -0600

    replaced references with nuget references.  added a nunit unit test that runs all mspec tests.

---
+++ Source/DynamicLinq.Specs/MSpecRunnerForNUnit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Machine.Specifications;
+using NUnit.Framework;
+
+namespace DynamicLinq
+{
+	[TestFixture]
+	public class MSpecRunnerForNUnit
+	{
+		private IEnumerable<object[]> TestFixtures
+		{
+			get
+			{
+				return typeof (MSpecRunnerForNUnit).Assembly.GetTypes()
+					.Select(x => new {Type = x, Fields = x.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly)})
+					.Where(x => x.Fields.Any(y => y.FieldType == typeof (It)))
+					.Select(x => new object[] {x.Type, x.Fields});
+			}
+		}
+		
+		[Test]
+		[TestCaseSource("TestFixtures")]
+		public void Run_Machine_Specification(Type type, FieldInfo[] fields)
+		{
+			var contextField = fields.Single(x => x.FieldType == typeof (Establish));
+			var ofField = fields.Single(x => x.FieldType == typeof (Because));
+			var testFields = fields.Where(x => x.FieldType == typeof (It));
+
+			var testFixtureInstance = Activator.CreateInstance(type);
+
+			foreach (var testField in testFields)
+			{
+				if (contextField != null)
+				{
+					var context = (Establish) contextField.GetValue(testFixtureInstance);
+
+					context();
+				}
+
+				if (ofField != null)
+				{
+					var of = (Because) ofField.GetValue(testFixtureInstance);
+
+					of();
+				}
+
+				var test = (It) testField.GetValue(testFixtureInstance);
+
+				test();
+			}
+		}
+	}
+}
