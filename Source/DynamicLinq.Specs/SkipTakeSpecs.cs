52e6dc7fe0034f26064085b9963df81c9de52ebf
Brandon Barry <maydaytx@gmail.com>
2012-01-21 21:15:28 -0600

    removed azure tables implementation

--- Source/DynamicLinq.Specs/SkipTakeSpecs.cs
+++ Source/DynamicLinq.Specs/SkipTakeSpecs.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Services.Client;
+﻿using System.Collections.Generic;
 using System.Linq;
-using DynamicLinq.AzureTables;
 using DynamicLinq.Databases;
 using Machine.Specifications;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.StorageClient;
 
 namespace DynamicLinq
 {
@@ -113,110 +108,5 @@ namespace DynamicLinq
 
 		It should_retrieve_2_records = () =>
 			results.Count.ShouldEqual(2);
-	}
-
-	public class When
-	{
-		private static AzureTablesDB db;
-		private static IList<dynamic> results;
-
-		public class TestTable : TableServiceEntity
-		{
-			public string WTF { get; set; }
-			public int ASDF { get; set; }
-		}
-
-		Establish context = () =>
-		{
-			CloudTableClient client = new CloudTableClient(CloudStorageAccount.DevelopmentStorageAccount.TableEndpoint.AbsoluteUri, CloudStorageAccount.DevelopmentStorageAccount.Credentials);
-
-			//client.DeleteTableIfExist("TestTable");
-			//client.CreateTable("TestTable");
-
-			//var context = client.GetDataServiceContext();
-
-			//context.AddObject("TestTable", new TestTable { PartitionKey = "1", RowKey = "1", WTF = "Bool", ASDF = 4 });
-			//context.AddObject("TestTable", new TestTable { PartitionKey = "1", RowKey = "2", WTF = "Bool", ASDF = 1 });
-			//context.AddObject("TestTable", new TestTable { PartitionKey = "1", RowKey = "3", WTF = null, ASDF = 7 });
-
-			//context.SaveChanges(SaveChangesOptions.Batch);
-
-			//var tests = client.GetDataServiceContext().CreateQuery<TestTable>("TestTable").Select(x => x).ToList();
-
-			db = new AzureTablesDB(CloudStorageAccount.DevelopmentStorageAccount);
-
-			db.Update(x => x.TestTable)
-				.With(new AzureTablesObject("1", "1", new {WTF = "LOOB", ASDF = 4})
-					//,new AzureTablesObject("1", "2", new {WTF = "Bool", ASDF = 1})
-					//,new AzureTablesObject("1", "3", new {WTF = default(string), ASDF = 7})
-					);
-		};
-
-		Because of = () =>
-		{
-			results = (from record in db.Query(x => x.TestTable)
-					   //where record.WTF == "Bool"// && record.ASDF == 1
-					   //where record.PartitionKey == "1" && record.RowKey == "2"
-					   select record).ToList();
-		};
-
-		It should_retrieve_the_records = () =>
-		{
-		};
-	}
-
-	public class When2
-	{
-		private static AzureTablesDB db;
-		private static IList<dynamic> results;
-
-		private static string Reverse(IEnumerable<char> str)
-		{
-			return new string(str.Reverse().ToArray());
-		}
-
-		Establish context = () =>
-		{
-			//db = new AzureTablesDB(new CloudStorageAccount(CloudStorageAccount.DevelopmentStorageAccount.Credentials, null, null, new Uri("http://127.0.0.1:10457/devstoreaccount1")));
-			db = new AzureTablesDB(CloudStorageAccount.DevelopmentStorageAccount);
-
-			results = db.Query(x => x.Foo).ToList();
-
-			db.Update(x => x.Foo).With(new AzureTablesObject("asdf", "12", new {Bar = Reverse(results[0].Bar), ASDF = results[0].ASDF.AddYears(1)}));
-		};
-
-		Because of = () =>
-		{
-		};
-
-		It should_retrieve_the_records = () =>
-		{
-		};
-	}
-
-	public class When3
-	{
-		private static AzureTablesDB db;
-
-		Establish context = () =>
-		{
-			CloudTableClient client = new CloudTableClient(CloudStorageAccount.DevelopmentStorageAccount.TableEndpoint.AbsoluteUri, CloudStorageAccount.DevelopmentStorageAccount.Credentials);
-
-			client.DeleteTableIfExist("Foo");
-			client.CreateTable("Foo");
-
-			//db = new AzureTablesDB(new CloudStorageAccount(CloudStorageAccount.DevelopmentStorageAccount.Credentials, null, null, new Uri("http://127.0.0.1:10457/devstoreaccount1")));
-			db = new AzureTablesDB(CloudStorageAccount.DevelopmentStorageAccount);
-
-			db.Insert(new AzureTablesObject("asdf", "12", new {Bar = "Bool", ASDF = DateTime.Now})).Into(x => x.Foo);
-		};
-
-		Because of = () =>
-		{
-		};
-
-		It should_retrieve_the_records = () =>
-		{
-		};
 	}
 }
